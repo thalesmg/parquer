c822fad41f072110280b7983dbbafe9e618f51a9
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-06-06 16:33:57 -0300

    test: improve thrift utils coverage

---
+++ test/parquer_thrift_utils_tests.erl
@@ -0,0 +1,90 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2025 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+-module(parquer_thrift_utils_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("parquer.hrl").
+-include("parquer_parquet_types.hrl").
+
+%%------------------------------------------------------------------------------
+%% Type declarations
+%%------------------------------------------------------------------------------
+
+all_converted_types() ->
+  [ ?CONVERTED_TYPE_LIST
+  , ?CONVERTED_TYPE_UTF8
+  , ?CONVERTED_TYPE_ENUM
+  , ?CONVERTED_TYPE_MAP
+  , ?CONVERTED_TYPE_MAP_KEY_VALUE
+  ].
+
+logical_type(T) ->
+  logical_type(T, #{}).
+logical_type(T, Extra) ->
+  Extra#{?name => T}.
+
+all_logical_types() ->
+  TimeUnit1 = #'timeUnit'{mILLIS = #'milliSeconds'{}},
+  TimeUnit2 = #'timeUnit'{nANOS =  #'nanoSeconds'{}},
+  [ logical_type(?lt_string)
+  , logical_type(?lt_list)
+  , logical_type(?lt_map)
+  , logical_type(?lt_enum)
+  , logical_type(?lt_date)
+  , logical_type(?lt_uuid)
+  , logical_type(?lt_unknown)
+  , logical_type(?lt_json)
+  , logical_type(?lt_bson)
+  , logical_type(?lt_float16)
+  , logical_type(?lt_time, #{?unit => TimeUnit1, ?is_adjusted_to_utc => true})
+  , logical_type(?lt_timestamp, #{?unit => TimeUnit2, ?is_adjusted_to_utc => false})
+  , logical_type(?lt_int, #{?bit_width => 3, ?is_signed => true})
+  , logical_type(?lt_variant, #{?specification_version => 10})
+  , logical_type(?lt_geometry, #{?crs => <<"crs">>})
+  , logical_type(?lt_geography, #{?crs => <<"crs">>, ?algorithm => <<"algo">>})
+  ].
+
+schema_element_params(LogicalType, ConvertedType) ->
+  #{ ?name => <<"col">>
+   , ?repetition => ?REPETITION_REQUIRED
+   , ?logical_type => LogicalType
+   , ?converted_type => ConvertedType
+   }.
+
+%%------------------------------------------------------------------------------
+%% Helper fns
+%%------------------------------------------------------------------------------
+
+%% Only to check we have clauses for all constants; i.e., it doesn't crash.
+logical_type_coverage_test_() ->
+  [ ?_test(
+       parquer_thrift_utils:schema_element(schema_element_params(LogicalType, ?undefined))
+      )
+  || LogicalType <- all_logical_types()
+  ].
+
+%% Only to check we have clauses for all constants; i.e., it doesn't crash.
+converted_type_coverage_test_() ->
+  [ ?_test(
+       parquer_thrift_utils:schema_element(schema_element_params(?undefined, ConvertedType))
+      )
+  || ConvertedType <- all_converted_types()
+  ].
+
+%%%_* Emacs ====================================================================
+%%% Local Variables:
+%%% erlang-indent-level: 2
+%%% End:
